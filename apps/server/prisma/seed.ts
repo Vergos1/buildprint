8aa2145369c4789c95c8cc772cb9df6d09e51802
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-09 18:55:03 +0300

    feat: enhance Prisma configuration with seeding functionality and update User model to include role

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -1,34 +1,48 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import * as bcrypt from 'bcrypt';
+import 'dotenv/config';
+import { PrismaClient } from '../src/generated/prisma/client';
 
-// initialize the Prisma Client
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+const prisma = new PrismaClient({ adapter });
 
 const roundsOfHashing = 10;
 
 async function main() {
+  // create two dummy users
   const passwordSabin = await bcrypt.hash('password-sabin', roundsOfHashing);
   const passwordAlex = await bcrypt.hash('password-alex', roundsOfHashing);
 
-  const user1 = await prisma.user.upsert({
-    where: { email: 'sabin@adams.com' },
-    update: {
-      password: passwordSabin,
-    },
-    create: {
+  const user1 = await prisma.user.create({
+    data: {
       email: 'sabin@adams.com',
       name: 'Sabin Adams',
       password: passwordSabin,
     },
   });
 
-  const user2 = await prisma.user.upsert({
-    where: { email: 'alex@ruheni.com' },
-    update: { password: passwordAlex },
-    create: {
+  const user2 = await prisma.user.create({
+    data: {
       email: 'alex@ruheni.com',
       name: 'Alex Ruheni',
       password: passwordAlex,
     },
   });
+
+  console.log({ user1, user2 });
 }
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close the Prisma Client at the end
+    await prisma.$disconnect();
+  });
