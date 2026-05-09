04385a37665c0f057433275ea70cd3965410fda6
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-09 15:57:32 +0300

    feat: add UsersModule to app.module for user management functionality

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
