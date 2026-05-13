6456498daf64ab1328704fb09b32340c5dabe112
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-14 02:02:00 +0300

    refactor: remove useLogin hook and enhance useRegister with navigation and error handling

--- apps/web/src/features/auth/hooks/use-register.ts
+++ apps/web/src/features/auth/hooks/use-register.ts
@@ -1,10 +1,19 @@
 import { MUTATION_KEYS } from "@shared-config/query-keys"
 import { useMutation } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
 import { authApi } from "../api"
 
 export function useRegister() {
+  const router = useRouter()
+
   return useMutation({
     mutationKey: MUTATION_KEYS.auth.register,
     mutationFn: authApi.register,
+    onSuccess: () => {
+      router.push("/login")
+    },
+    onError: (error) => {
+      console.error("Помилка реєстрації:", error)
+    },
   })
 }
