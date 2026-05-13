a2dda51128b3a1557c37a61670cb681697238b66
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-14 01:19:14 +0300

    refactor: update base URL in axios instance to use root API path for improved flexibility

--- apps/web/src/api/instance.ts
+++ apps/web/src/api/instance.ts
@@ -6,7 +6,7 @@ import axios, {
   RawAxiosRequestHeaders,
 } from "axios"
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`
+const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 
 const instance = axios.create({
   baseURL: BASE_URL,
