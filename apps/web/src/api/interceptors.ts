e3e72258636680a405ff94a12b8029c00cec2964
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-15 21:41:49 +0300

    refactor: enhance request interceptor to use shared auth method for server-side authentication

--- apps/web/src/api/interceptors.ts
+++ apps/web/src/api/interceptors.ts
@@ -1,8 +1,13 @@
+import { auth } from "@shared-lib"
 import { getSession } from "next-auth/react"
 import { instance } from "./instance"
 
+const isServer = () => typeof window === "undefined"
+
 instance.interceptors.request.use(async (config) => {
-  const session = await getSession()
+  const session =
+    typeof window === "undefined" ? await auth() : await getSession()
+
   if (session?.accessToken) {
     config.headers.Authorization = `Bearer ${session.accessToken}`
   }
