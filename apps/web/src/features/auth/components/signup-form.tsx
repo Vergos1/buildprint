d97a47eeef93184ca919d970228694aacc860b80
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-15 19:27:47 +0300

    feat: implement signup form with validation and error handling using react-hook-form and Zod

--- apps/web/src/features/auth/components/signup-form.tsx
+++ apps/web/src/features/auth/components/signup-form.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
 import Abstraction from "@public/auth/abstraction-2.png"
 import {
   Button,
@@ -5,79 +7,150 @@ import {
   CardContent,
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
   Input,
+  MessageBlock,
 } from "@workspace-components"
 import { cn } from "@workspace-lib"
 import Image from "next/image"
 import Link from "next/link"
+import { Controller, useForm } from "react-hook-form"
 import { links } from "src/shared/config"
 import { PrivacyBlock } from "../../../components/privacy-block"
+import { useRegister } from "../hooks/use-register"
+import { RegisterSchema, registerSchema } from "../schema"
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const { mutate: register, isPending, error } = useRegister()
+
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  })
+
+  const onSubmit = (dto: RegisterSchema) => {
+    register(dto)
+  }
+
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn("flex flex-col gap-5", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
-            <FieldGroup>
+          <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup className="gap-6">
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Реєстрація</h1>
                 <p className="text-sm text-balance text-muted-foreground">
                   Введіть дані нижче, щоб створити новий акаунт
                 </p>
               </div>
-              <Field>
-                <FieldLabel htmlFor="name">Ім&apos;я</FieldLabel>
-                <Input
-                  id="name"
-                  type="name"
-                  placeholder="Іван Петренко"
-                  required
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor="email">Електронна пошта</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="ivan@gmail.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <Field className="grid grid-cols-2 gap-4">
+
+              {error && (
+                <MessageBlock>{error.response?.data?.message}</MessageBlock>
+              )}
+
+              <Controller
+                name="name"
+                control={form.control}
+                render={({ field, fieldState }) => (
                   <Field>
-                    <FieldLabel htmlFor="password">Пароль</FieldLabel>
+                    <FieldLabel htmlFor={field.name}>Ім&apos;я</FieldLabel>
                     <Input
-                      id="password"
-                      type="password"
-                      placeholder="••••••••"
-                      required
-                    />
+                      {...field}
+                      id={field.name}
+                      type={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Іван Петренко"
+                    />{" "}
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
                   </Field>
+                )}
+              />
+
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
                   <Field>
-                    <FieldLabel htmlFor="confirm-password">
-                      Повторіть пароль
+                    <FieldLabel htmlFor={field.name}>
+                      Електронна пошта
                     </FieldLabel>
                     <Input
-                      id="confirm-password"
-                      type="password"
-                      placeholder="••••••••"
-                      required
+                      {...field}
+                      id={field.name}
+                      name={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="ivan@gmail.com"
                     />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
                   </Field>
+                )}
+              />
+              <Field>
+                <Field className="grid grid-cols-2 gap-4">
+                  <Controller
+                    name="password"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field>
+                        <FieldLabel htmlFor={field.name}>Пароль</FieldLabel>
+                        <Input
+                          {...field}
+                          id={field.name}
+                          type="password"
+                          aria-invalid={fieldState.invalid}
+                          placeholder="••••••••"
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                  <Controller
+                    name="confirmPassword"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field>
+                        <FieldLabel htmlFor={field.name}>
+                          Повторіть пароль
+                        </FieldLabel>
+                        <Input
+                          {...field}
+                          id={field.name}
+                          type="password"
+                          aria-invalid={fieldState.invalid}
+                          placeholder="••••••••"
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
                 </Field>
                 <FieldDescription>
                   Пароль має містити щонайменше 8 символів.
                 </FieldDescription>
               </Field>
               <Field>
-                <Button type="submit">Зареєструватись</Button>
+                <Button type="submit" disabled={isPending}>
+                  {isPending ? "Загрузка" : "Зареєструватись"}
+                </Button>
               </Field>
               <FieldDescription className="text-center">
                 Вже є акаунт? <Link href={links.login}>Увійти</Link>
