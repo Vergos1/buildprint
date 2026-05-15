69b5e05ce81446fe8d754c7c2951963391357932
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-15 19:27:55 +0300

    refactor: update authentication method in API interceptors to use next-auth's getSession

--- apps/web/src/api/interceptors.ts
+++ apps/web/src/api/interceptors.ts
@@ -1,8 +1,8 @@
-import { auth } from "@shared-lib"
+import { getSession } from "next-auth/react"
 import { instance } from "./instance"
 
 instance.interceptors.request.use(async (config) => {
-  const session = await auth()
+  const session = await getSession()
   if (session?.accessToken) {
     config.headers.Authorization = `Bearer ${session.accessToken}`
   }
