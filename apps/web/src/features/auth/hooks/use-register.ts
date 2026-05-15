eb0da50506952244a5bd81d384c92ec246af52b7
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-15 20:18:19 +0300

    refactor: change import statement for ApiErrorResponse to use type import for consistency

--- apps/web/src/features/auth/hooks/use-register.ts
+++ apps/web/src/features/auth/hooks/use-register.ts
@@ -2,7 +2,7 @@ import { MUTATION_KEYS } from "@shared-config/query-keys"
 import { useMutation } from "@tanstack/react-query"
 import { AxiosError } from "axios"
 import { useRouter } from "next/navigation"
-import { ApiErrorResponse } from "src/shared/types/api-error"
+import type { ApiErrorResponse } from "src/shared/types/api-error"
 import { authApi } from "../api"
 
 export function useRegister() {
