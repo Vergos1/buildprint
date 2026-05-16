6a482b9545dd0b06d52acaa6276c46cec4d54e70
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-16 23:58:11 +0300

    feat: add useNetworkStatus hook to monitor online status and export from hooks index

---
+++ apps/web/src/shared/hooks/use-network-status.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+const useNetworkStatus = () => {
+  const [isOnline, setOnline] = useState<boolean>(true)
+
+  const updateNetworkStatus = () => {
+    setOnline(navigator.onLine)
+    console.log(isOnline)
+  }
+
+  useEffect(() => {
+    window.addEventListener("load", updateNetworkStatus)
+    window.addEventListener("online", updateNetworkStatus)
+    window.addEventListener("offline", updateNetworkStatus)
+    return () => {
+      window.removeEventListener("load", updateNetworkStatus)
+      window.removeEventListener("online", updateNetworkStatus)
+      window.removeEventListener("offline", updateNetworkStatus)
+    }
+  }, [navigator.onLine])
+  return { isOnline }
+}
+
+export default useNetworkStatus
