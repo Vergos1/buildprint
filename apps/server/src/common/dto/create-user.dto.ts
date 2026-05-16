61543d6bad5c86d5dcbe52185c6fe89bc877373f
Ihor Yanchuk <legendsmobajl4@gmail.com>
2026-05-16 16:04:42 +0300

    refactor: update password validation in CreateUserDto to require a minimum of 8 characters for enhanced security

--- apps/server/src/common/dto/create-user.dto.ts
+++ apps/server/src/common/dto/create-user.dto.ts
@@ -14,7 +14,7 @@ export class CreateUserDto {
 
   @IsString({ message: 'Пароль має бути рядком' })
   @IsNotEmpty({ message: 'Пароль не може бути порожнім' })
-  @MinLength(6, { message: 'Пароль має містити мінімум 6 символів' })
-  @ApiProperty({ example: '123456', minLength: 6 })
+  @MinLength(6, { message: 'Пароль має містити мінімум 8 символів' })
+  @ApiProperty({ example: '123456', minLength: 8 })
   password!: string;
 }
